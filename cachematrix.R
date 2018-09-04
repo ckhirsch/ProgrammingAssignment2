99cba79397bf99a87b27e65b8dfe52f2dfefe84e
ckhirsch <ckh@mit.edu>
2018-09-04 14:00:18 -0700

    Hirsch Edited File
    Programming Assignment 2 - Hirsch Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: creates a special "matrix" that can cache its inverse.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrix_inverse = NULL
+        set = function(y) {
+                x <<- y
+                matrix_inverse <<- NULL
+        }
+        get = function() {
+                x
+        }
+        setinv = function(inverse) {
+                matrix_inverse <<- inverse 
+        }
+        getinv = function() {
+                matrix_inverse
+        }
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+##cacheSolve: returns the inverse of the matrix returned by makeCacheMatrix(); 
+##            if the inverse has previously been computed and has not changed, 
+##            the already cached solution will be retrieved
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        matrix_inverse = x$getinv()
+        if (!is.null(matrix_inverse)){
+                message("getting cached data")
+                return(matrix_inverse)
+        }
+        data = x$get()
+        matrix_inverse = solve(data, ...)
+        x$setinv(matrix_inverse)
+        matrix_inverse
 }
